df71b1365c9c6fde6e719c7c0f74c47a6436b078
Olivier Van hamme <nicefiction@gmail.com>
2022-04-08 22:16:58 +0200

    Completes the Challenge of Day 25: Rock Paper Scissors.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -1,21 +1,159 @@
-//
-//  ContentView.swift
-//  RockPaperScissors
-//
-//  Created by Olivier Van hamme on 08/04/2022.
-//
+/// Source:
+/// https://www.hackingwithswift.com/guide/ios-swiftui/2/3/challenge
 
 import SwiftUI
 
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    // MARK: - PROPERTY WRAPPERS
+    @State private var currentGameRound = 0
+    @State private var gameOptions: Array<String> = [
+        "🪨", "📄", "✂️"
+    ]
+    @State private var optionSelectedByGame = Int.random(in: 0...2)
+    @State private var gameScore: Int = 0 {
+        didSet {
+            if gameScore < 0 {
+                gameScore = 0
+            }
+        }
     }
+    @State private var gameFeedback: String = ""
+    @State private var isShowingEndScoreAlert: Bool = false
+    
+    
+    
+    // MARK: - PROPERTIES
+    let maxGameRound: Int = 3
+    let challengeText: Text = Text("Your Pick?")
+    var optionSelectedByHuman: Int = 0
+    
+    
+    
+    // MARK: - INITIALIZERS
+    // MARK: - COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        VStack(spacing: 50) {
+            VStack(spacing: 10) {
+                Text("I have chosen.")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                challengeText
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+            }
+            HStack {
+                ForEach(gameOptions,
+                        id: \.self) { (option: String) in
+                    Button {
+                        print("\(option) was tapped.")
+                        determineResult(with: option)
+                        startNewRound()
+                        if currentGameRound == maxGameRound {
+                            isShowingEndScoreAlert.toggle()
+                        }
+                    } label: {
+                        Text(option)
+                            .font(.system(size: 75))
+                            .padding(8)
+                    }
+                }
+            }
+            Text(gameFeedback)
+                .font(.title)
+                .fontWeight(.medium)
+        }
+        .alert("Your end score is \(gameScore).",
+               isPresented: $isShowingEndScoreAlert) {
+            Button("Start New Game.",
+                   role: .none,
+                   action: startNewGame)
+        }
+    }
+    
+    
+    
+    // MARK: - METHODS
+    
+    func determineResult(with option: String)
+    -> Void {
+        
+        switch (gameOptions[optionSelectedByGame], option) {
+        case ("🪨", "🪨"):
+            gameScore += 0
+            gameFeedback = "Rocks cancel each other out, try again."
+            
+        case ("🪨", "📄"):
+            gameScore += 1
+            gameFeedback = "Paper envelops rock, you win."
+            
+        case ("🪨", "✂️"):
+            gameScore -= 1
+            gameFeedback = "Rock crushes scissors, you loose."
+            
+        case ("📄", "🪨"):
+            gameScore -= 1
+            gameFeedback = "Paper envelops rock, you loose."
+            
+        case ("📄", "📄"):
+            gameScore += 0
+            gameFeedback = "Papers cancel each other out, try again."
+            
+        case ("📄", "✂️"):
+            gameScore += 1
+            gameFeedback = "Scissors cut paper, you win."
+            
+        case ("✂️", "🪨"):
+            gameScore += 1
+            gameFeedback = "Rock crushes scissors, you win."
+            
+        case ("✂️", "📄"):
+            gameScore -= 1
+            gameFeedback = "Scissors cut paper, you loose."
+            
+        default:
+            gameScore += 0
+            gameFeedback = "Scissors cancel each other out, try again."
+        }
+    }
+    
+    
+    func startNewRound()
+    -> Void {
+        
+        optionSelectedByGame = Int.random(in: 0...2)
+        currentGameRound += 1
+    }
+    
+    
+    func startNewGame()
+    -> Void {
+        
+        currentGameRound = 0
+        optionSelectedByGame = Int.random(in: 0...2)
+    }
+    
+    
+    
+    // MARK: - HELPER METHODS
 }
 
+
+
+
+
+// MARK: - PREVIEWS
+
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        ContentView()
+        
+        Group {
+            ContentView().preferredColorScheme(.dark)
+        }
     }
 }
